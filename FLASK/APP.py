431e193164e4b533f4c59c15d210cc5a40c3b054
Shivansh Tiwari <shivanshtiwari3325@gmail.com>
2025-07-10 12:07:30 +0530

    ..

--- FLASK/APP.py
+++ FLASK/APP.py
@@ -6,12 +6,14 @@ app.secret_key_ = 'supersecretmre'
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/about')
-def about():
-    return render_template('about.html')
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000, debug=True)  
