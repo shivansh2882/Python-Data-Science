b2ced0bbd5967529c50fd118507b1cf4b7f4c594
Shivansh Tiwari <shivanshtiwari3325@gmail.com>
2025-07-11 18:12:57 +0530

    ..

--- FLASK/APP.py
+++ FLASK/APP.py
@@ -16,4 +16,4 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000, debug=True)  
+    app.run(host = '0.0.0.0', port = 5000, debug=True) 
